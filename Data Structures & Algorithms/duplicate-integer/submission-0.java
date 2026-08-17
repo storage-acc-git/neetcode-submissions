a686496291ca4b24708f50437d359e1de46a8c71
storage-acc-git <storage.acc.git@gmail.com>
2026-08-17 09:22:03 +0300

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int x : nums) {
+            if(!set.add(x)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
