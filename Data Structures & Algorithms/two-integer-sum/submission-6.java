b8dbc738a839dc07f9d64b24c354fdf342b6cadc
storage-acc-git <storage.acc.git@gmail.com>
2026-08-17 10:33:43 +0300

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> values = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (values.containsKey(target - nums[i])) {
+                return new int[] { values.get(target - nums[i]), i };
+            }
+
+            values.put(nums[i], i);
+        }
+
+        return new int[0];
+    }
+}
